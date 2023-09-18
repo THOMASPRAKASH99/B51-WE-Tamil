da16b927a48a04e387bb27fedb0a504b82060fec
THOMASPRAKASH99 <thomasprakash999@gamil.com>
2023-09-18 23:27:12 +0530

    task

--- js-day3/2qustion.js
+++ js-day3/2qustion.js
@@ -1,36 +1,37 @@
-//(A) arrow function
+// (A) arrow function
 
-// let funs = (arr) => {
-//         for (var i=0; i<arr.length; i++){
-//             if(arr[i]%2!==0)
-//             console.log(arr[i]);
-//         }
-//     }
-//     ([1,2,3,4,5,6,7,8,9])
+let funs = (arr) => {
+        for (var i=0; i<arr.length; i++){
+            if(arr[i]%2!==0)
+            console.log(arr[i]);
+        }
+    }
+    ([1,2,3,4,5,6,7,8,9])
 
 
 // (b) Convert all the strings to title caps in a string array
     
-        //  const array = (arr) => {
-        //     for (let i=0; i < arr.length; i++) {
-        //       arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
-        //       console.log(arr);
-        //       }
-        //     }(["india","is","our","country"])
+         const array = (arr) => {
+            for (let i=0; i < arr.length; i++) {
+              arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+              console.log(arr);
+              }
+            }
+            arr("india is our country")
 
 
 //    (c) Sum of all numbers in an array
 
-// var add = (num) => { 
-//     let sum = 0;
+var add = (num) => { 
+    let sum = 0;
     
-//     for (let i=0; i < num.length; i++)
-//     sum += num[i];
-//   return sum;
-//   }
-//   var num = [1,2,3,4,5];
-//   console.log(add(num));
+    for (let i=0; i < num.length; i++)
+    sum += num[i];
+  return sum;
+  }
+  var num = [1,2,3,4,5];
+  console.log(add(num));
 
 
-funtion(a,d)
+
 
